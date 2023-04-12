b97f0e2a4b3d6e6348ee2ca1119c6c4aec888111
Bamidele joshua olayinka <Olayinka775@gmail.com>
2023-04-12 11:52:58 +0100

    SignUp

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Navbar from './components/Navbar'
 import Corders from './components/Corders'
 import Second from './components/Second'
 import Fetch from './components/Fetch'
-import {Route, Routes, BrowserRouter as Router} from 'react-router-dom'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
 // import useState from 'react'
 
@@ -15,40 +15,15 @@ function App() {
 
   return (
     <>
-       {/* <Navbar/>
-       <Corders /> 
-       <Second />     */}
-       {/* <Fetch/> */}
-
-       <Route>
+       <Navbar/>
+       <BrowserRouter>
          <Routes>
-           <Route path=''></Route>
+           <Route path="/" element={<Fetch/>}/>
+           <Route path="/SignUp" element={<SignUp/>}/>
          </Routes>
-       </Route>
+       </BrowserRouter>
        
     </>
-    // <div className="App">
-    //   <div>
-    //     <a href="https://vitejs.dev" target="_blank">
-    //       <img src={viteLogo} className="logo" alt="Vite logo" />
-    //     </a>
-    //     <a href="https://reactjs.org" target="_blank">
-    //       <img src={reactLogo} className="logo react" alt="React logo" />
-    //     </a>
-    //   </div>
-    //   <h1>Josh & Codist + React</h1>
-    //   <div className="card">
-    //     <button onClick={() => setCount((count) => count + 1)}>
-    //       count is {count}
-    //     </button>
-    //     <p>
-    //       Edit <code>src/App.jsx</code> and save to test HMR
-    //     </p>
-    //   </div>
-    //   <p className="read-the-docs">
-    //     Click on the Vite and React logos to learn more
-    //   </p>
-    //</div>
   )
 }
 
